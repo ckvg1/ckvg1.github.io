eee77439d79830fb7e94ae95d98006fa1570c89d
blukasik77 <kfckvgjv59@gmail.com>
2025-08-12 10:13:38 +0200

    lepszy wykres tymperatury

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,28 +1,31 @@
 const local = localStorage.getItem("theme");
-if(local === "ciemny-motyw"){
-  document.querySelectorAll("body, header, .modal-content, .hamburger, .tab-button, .oaerror, strong, section, .przycisk, input, select, .icons_div, .bar1, .bar2, .bar3, .nazwaBiura, .temperatura").forEach((el) => {
-    el.classList.add("ciemny");
-  });
-
+if (local === "ciemny-motyw") {
+  document
+    .querySelectorAll(
+      "body, header, .modal-content, .hamburger, .tab-button, .oaerror, strong, section, .przycisk, input, select, .icons_div, .bar1, .bar2, .bar3, .nazwaBiura, .temperatura"
+    )
+    .forEach((el) => {
+      el.classList.add("ciemny");
+    });
 
   document.getElementById("settings_icon").classList.add("ciemny");
 
-  document.getElementById("settings_icon").src = "./img_main/icony/settings-dark.png";
-  document.getElementById("dark-mode").src = "img_main/icony/dark-mode-dark.png"
+  document.getElementById("settings_icon").src =
+    "./img_main/icony/settings-dark.png";
+  document.getElementById("dark-mode").src =
+    "img_main/icony/dark-mode-dark.png";
 }
 
-function darkMode(){
+function darkMode() {
   body = document.querySelectorAll("body");
 
   body.forEach((el) => {
     el.classList.toggle("ciemny");
   });
 
-  if (document.body.classList.contains("ciemny")){
-      localStorage.setItem("theme", "ciemny-motyw");
-  }
-  else if(document.body.classList.contains("jasny")){
-      localStorage.setItem("theme", "jasny-motyw");
+  if (document.body.classList.contains("ciemny")) {
+    localStorage.setItem("theme", "ciemny-motyw");
+  } else if (document.body.classList.contains("jasny")) {
+    localStorage.setItem("theme", "jasny-motyw");
   }
 }
-
